6044cc4c1e3a2c81adfa830afb4af2cdc24be5ce
Bussler <maarten.bussler@gmail.com>
2023-10-14 09:33:17 +0200

    Setup

---
+++ run_nerf.py
@@ -0,0 +1,14 @@
+import os
+from typing import Optional, Tuple, List, Union, Callable
+
+import numpy as np
+import torch
+from torch import nn
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from tqdm import trange
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+print(f"Running on: {device}")
