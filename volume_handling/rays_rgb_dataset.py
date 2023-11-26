4153342401acced3e61850b36e1e181e092c6860
Bussler <maarten.bussler@gmail.com>
2023-11-26 22:08:23 +0100

    refactor dataset class

--- volume_handling/rays_rgb_dataset.py
+++ volume_handling/rays_rgb_dataset.py
@@ -22,19 +22,20 @@ class Ray_Rgb_Dataset(Dataset):
         """
         self.rays_rgb = rays_rgb
         self.batch_size = batch_size
+        self.ibatch = 0
 
     def __len__(self):
         return self.rays_rgb.shape[0] // self.batch_size
 
     def __getitem__(self, index):
         # In batch_size steps over rays.
-        batch = self.rays_rgb[index * self.batch_size : (index + 1) * self.batch_size]
+        batch = self.rays_rgb[self.ibatch : self.ibatch + self.batch_size]
         batch = torch.transpose(batch, 0, 1)
-        index += 1
+        self.ibatch += self.batch_size
         # Shuffle after one epoch
-        if (index + 1) * self.batch_size >= self.rays_rgb.shape[0]:
+        if self.ibatch >= self.rays_rgb.shape[0]:
             self.rays_rgb = self.rays_rgb[torch.randperm(self.rays_rgb.shape[0])]
-            index = 0
+            self.ibatch = 0
 
         rays_o, rays_d, target_img = batch
-        return rays_o, rays_d, target_img, index
+        return rays_o, rays_d, target_img
